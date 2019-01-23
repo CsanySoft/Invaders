bb205326b47457668a81929a677581c1ec167262
Dávid Mátyás <davimatyi@outlook.com>
2019-01-23 17:34:28 +0100

    röpködnek a süti kövek

---
+++ core/src/hu/csanysoft/invaders/Actors/Enemy.java
@@ -0,0 +1,35 @@
+package hu.csanysoft.invaders.Actors;
+
+
+import java.util.Random;
+
+import hu.csanysoft.invaders.Game.GameStage;
+import hu.csanysoft.invaders.Global.Assets;
+import hu.csanysoft.invaders.MyBaseClasses.Scene2D.MultiSpriteActor;
+import hu.csanysoft.invaders.MyBaseClasses.Scene2D.OffsetSprite;
+
+public class Enemy extends MultiSpriteActor {
+
+    float x, y;
+    Random rand;
+    float speed;
+    float timer = 2;
+    float moveTime = 0;
+
+    public Enemy(float x, float y) {
+        super(300,300);
+
+        addBaseCollisionRectangleShape();
+        setPosition(x,y);
+        this.x = x; this.y = y;
+        rand = new Random();
+        speed = rand.nextInt(2)+1;
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+
+    }
+}
