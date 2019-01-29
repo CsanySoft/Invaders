ce52a80c061e31ba2815f59bbe76d6306fb1c019
Dávid Mátyás <davimatyi@outlook.com>
2019-01-29 16:01:57 +0100

    picisüti 2.0

--- core/src/hu/csanysoft/invaders/Actors/SubMeteorite.java
+++ core/src/hu/csanysoft/invaders/Actors/SubMeteorite.java
@@ -3,21 +3,24 @@ package hu.csanysoft.invaders.Actors;
 public class SubMeteorite extends Meteorite {
 
     private int direction;
+    private int moveX, moveY;
 
     public SubMeteorite(float x, float y, int direction) {
         super(x, y);
         this.direction = direction;
         setSize(30,30);
+        moveX = rand.nextInt(5);
+        moveY = rand.nextInt(5);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
         switch(direction){
-            case 1: moveBy(2,2); break;
-            case 2: moveBy(2, -2); break;
-            case 3: moveBy(-2, -2); break;
-            case 4: moveBy(-2, 2); break;
+            case 1: moveBy(moveX,moveY); break;
+            case 2: moveBy(moveX, -moveY); break;
+            case 3: moveBy(-moveX, -moveY); break;
+            case 4: moveBy(-moveX, moveY); break;
         }
     }
 }
--- core/src/hu/csanysoft/invaders/Global/Globals.java
+++ core/src/hu/csanysoft/invaders/Global/Globals.java
@@ -8,7 +8,7 @@ public class Globals {
     public static final int WORLD_WIDTH = 720;
     public static final int WORLD_HEIGHT = 1280;
 
-    public static final boolean DEBUG_ALL = true;
+    public static final boolean DEBUG_ALL = false;
 
     public static boolean ACCELEROMETER_AVAILABLE = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
 }
