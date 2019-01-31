902673b8c799e66d68fad438d3deb930c975bf88
Dávid Mátyás <davimatyi@outlook.com>
2019-01-31 18:08:18 +0100

    tutorial enhance

--- core/src/hu/csanysoft/invaders/Tutorial/TutorialStage.java
+++ core/src/hu/csanysoft/invaders/Tutorial/TutorialStage.java
@@ -2,16 +2,19 @@ package hu.csanysoft.invaders.Tutorial;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import hu.csanysoft.invaders.Game.SelectScreen;
 import hu.csanysoft.invaders.Global.Assets;
 import hu.csanysoft.invaders.Global.Globals;
 import hu.csanysoft.invaders.Invaders;
+import hu.csanysoft.invaders.MyBaseClasses.MyTextButton;
 import hu.csanysoft.invaders.MyBaseClasses.Scene2D.MyStage;
 import hu.csanysoft.invaders.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 import hu.csanysoft.invaders.MyBaseClasses.UI.MyLabel;
@@ -22,6 +25,7 @@ public class TutorialStage extends MyStage {
         super(viewport, batch, game);
         Gdx.input.setCatchBackKey(true);
         setDebugAll(Globals.DEBUG_ALL);
+        Gdx.input.setInputProcessor(this);
     }
 
     public void init() {
@@ -33,6 +37,7 @@ public class TutorialStage extends MyStage {
             }
 
         };
+        addActor(background);
 
         MyLabel label = new MyLabel("Irányításhoz döntse meg eszközét!\n" +
                 "Ha lőni akar tartsa ujját a képernyőn.\n" +
@@ -40,8 +45,34 @@ public class TutorialStage extends MyStage {
                 "és ha a hajónak ütköznek akkor meghal!" ,  Invaders.getColorLabelStyle(Color.YELLOW));
         addActor(label);
         label.setPosition((Globals.WORLD_WIDTH-label.getWidth())/2,(Globals.WORLD_HEIGHT-label.getHeight())/2+150);
+        addActor(new OneSpriteStaticActor(Assets.manager.get(Assets.TUTORIAL_TEXTURE)){
+            @Override
+            public void init() {
+                super.init();
+                setSize(300,400);
+                setPosition(Globals.WORLD_WIDTH/2-getWidth()/2, 250);
+            }
+        });
 
-        addActor(background);
+
+        addActor(new MyTextButton("Vissza"){
+            @Override
+            protected void init() {
+                super.init();
+                setPosition(Globals.WORLD_WIDTH/2-getWidth()/2, 50);
+                addListener(new ClickListener(){
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        super.clicked(event, x, y);
+                        game.setScreenBackByStackPop();
+                    }
+                });
+            }
+        });
+
+
+
+
         float size = (float)Math.sqrt(Globals.WORLD_WIDTH*Globals.WORLD_WIDTH + Globals.WORLD_HEIGHT*Globals.WORLD_HEIGHT);
         background.setSize(size,size);
         background.setPositionCenterOfActorToCenterOfViewport();
